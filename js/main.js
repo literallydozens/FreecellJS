6a1cbc3aa518de4bfcdf01aef3258bc300d62cfb
Joseph <lizeredman@gmail.com>
2024-07-06 17:44:23 +1200

    Added fullscreen button

--- js/main.js
+++ js/main.js
@@ -29,6 +29,30 @@ const NUMB_DICT = {
 
 // ==============================================================================================
 
+  /* View in fullscreen */
+  function openFullscreen() {
+    /* Get the documentElement (<html>) to display the page in fullscreen */
+    let elem = document.documentElement;
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+  }
+
+  /* Close fullscreen */
+  function closeFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+      document.msExitFullscreen();
+    }
+  }
+
 let lockScreen = (function(){
   let c = 0;
   return function(){
